52bf696fc745dd9316bc8ac36b9385aa7c8435e5
Guilherme <guimeirelesgmk@gmail.com>
2026-04-20 08:29:42 -0300

    feat:atividade sobre java iniciada

---
+++ scrip.js
@@ -0,0 +1,10 @@
+const catalogo=[
+{id:1, titulo:"Vingadores Ultimato",tipo: "filme",ano:2019,generos:["Ação","Herói"],nota:8.4,assistido:true},
+{id:2, titulo:"Peaky blinders",tipo:"série",ano:2012,generos:["Crime","Drama"],nota:8,assistido:true},
+{id:3, titulo:"The Batman",tipo: "filme",ano:2022,generos:["Ação","Crime"],nota:7.8,assistido:true},
+{id:4, titulo:"Pecadores",tipo: "filme",ano:2025,generos:["Ação","Drama"],nota:7.5,assistido:true},
+{id:5, titulo:"O mentalista",tipo: "serie",ano:2008,generos:["Drama","Crime"],nota:8.2,assistido:true},
+{id:6, titulo:"Top Gun Maverick",tipo: "filme",ano:2022,generos:["Drama","Ação"],nota:8.2,assistido:true},
+];
+
+console.log(catalogo[0].titulo,catalogo[5].ano,catalogo[2].generos[1]);
